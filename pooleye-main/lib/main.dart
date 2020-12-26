26e3daf0965189a6442568678085902636b15417
Islam Magdy <DashaDeshozz@gmail.com>
2020-12-26 23:20:17 +0200

    flutter initiative

--- pooleye-main/lib/main.dart
+++ pooleye-main/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:pooleye/view/login.dart';
+import 'package:pooleye/view/OrganizationProfile.dart';
 import 'package:pooleye/view/orgainsation_daily_reports.dart';
 
 void main() {
   runApp(new MaterialApp(
     title: "Dcode App",
-    home: new daily_report(),
+    home: new OrgProfile(),
   ));
 }

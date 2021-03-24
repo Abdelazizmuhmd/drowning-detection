abfc15f1fe96063ab505949bc89a2946e05d41f8
Islam Magdy <DashaDeshozz@gmail.com>
2021-03-24 14:59:43 +0200

    changes

--- pooleye-main/lib/model/lifeguard.dart
+++
@@ -1,7 +0,0 @@
-import 'package:pooleye/model/user.dart';
-
-class Lifeguard extends User {
-  String update() {
-    return "";
-  }
-}
--- pooleye-main/lib/model/medic.dart
+++
@@ -1,7 +0,0 @@
-import 'package:pooleye/model/user.dart';
-
-class Medic extends User {
-  String update() {
-    return "";
-  }
-}
